da327b31f32bbc14d162b245b0b1df20995fa5a6
sai-kiran-y <ysaikiran1997@gmail.com>
2024-09-09 14:31:09 +0530

    Fix to address unsed return value error in Yocto build

--- examples/peripheral/peripheral.c
+++ examples/peripheral/peripheral.c
@@ -603,7 +603,7 @@ void *read_imei_thread(void *arg) {
 	ssize_t n;
     while (1) {
         tcflush(tty_fd, TCIFLUSH);
-        write(tty_fd, at_cmd, strlen(at_cmd));
+        int write_ret = write(tty_fd, at_cmd, strlen(at_cmd));
         memset(buffer, 0, sizeof(buffer));
         usleep(100000);  // Small delay to allow the device to respond
 
