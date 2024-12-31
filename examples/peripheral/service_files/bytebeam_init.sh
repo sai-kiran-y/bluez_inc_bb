c91735532e54af91c21d1ce4d6ee9d6af37c06cf
sai-kiran-y <ysaikiran1997@gmail.com>
2024-12-31 10:40:39 +0530

    Add uplink and cancollector to bytebeam init. script

--- examples/peripheral/service_files/bytebeam_init.sh
+++ examples/peripheral/service_files/bytebeam_init.sh
@@ -4,3 +4,5 @@
 /home/root/ble_init.sh
 /etc/ppp/peers/ppp_on.sh
 /home/root/ble_app
+/home/root/uplink -c /home/root/config.toml -a /home/root/device.json -vvv
+/home/root/cancollector-hf -c can0 -s can_raw -a localhost:5050
